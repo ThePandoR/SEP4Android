a7898eae292bd1f3f02128fa7d435c5767b537cf
etlimss <pavilionytesmilte@gmail.com>
2020-10-30 17:29:20 +0100

    Main activity layout created

--- app/src/main/java/com/example/sep4_architecture/MainActivity.java
+++ app/src/main/java/com/example/sep4_architecture/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.sep4_architecture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.sep4_architecture.client.view.PagerAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PagerAdapter pagerAdapter;
+    private ViewPager viewPager;
+    private TabItem currentData, history, alerts;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        currentData = findViewById(R.id.currentData);
+        history = findViewById(R.id.history);
+        alerts = findViewById(R.id.alerts);
+
+        pagerAdapter = new
+                PagerAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
     }
 }
