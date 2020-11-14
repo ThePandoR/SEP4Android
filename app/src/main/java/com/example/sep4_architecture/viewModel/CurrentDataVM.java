73b438136b388c662ece0eed16174955d94f6eb1
etlimss <pavilionytesmilte@gmail.com>
2020-11-14 22:35:24 +0100

    updated current data view model

--- app/src/main/java/com/example/sep4_architecture/viewModel/CurrentDataVM.java
+++ app/src/main/java/com/example/sep4_architecture/viewModel/CurrentDataVM.java
@@ -24,7 +24,7 @@ public class CurrentDataVM extends ViewModel {
 
     // public CurrentDataVM(GreenHouseClient client) {
     public CurrentDataVM() {
-        this.client = new GreenHouseClient("http://192.168.137.1:8080/");
+        this.client = new GreenHouseClient("http://192.168.87.178:8080/");
         measurement = new MutableLiveData<Measurement>();
     }
 
